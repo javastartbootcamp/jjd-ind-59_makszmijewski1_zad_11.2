c59f7a7c946136212a9d5ac9d49627ef7c57ddd4
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-23 20:17:13 +0100

    pierwszy commit

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,20 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Person person1 = new Person("Jan", null, 25, "1234567890");
+            System.out.println(person1);
+        } catch (NameUndefinedException | IncorrectAgeException e) {
+            System.err.println(e.getMessage());
+        }
 
+        try {
+            Person person2 = new Person("Jan", "Kowalski", 25, "1234567890");
+            System.out.println(person2);
+            person2.setAge(-10);
+            System.out.println(person2);
+        } catch (NameUndefinedException | IncorrectAgeException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/pl/javastart/task/Person.java
@@ -0,0 +1,53 @@
+package pl.javastart.task;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String pesel;
+
+    public Person(String firstName, String lastName, int age, String pesel) {
+        if (checkName(firstName)) {
+            throw new NameUndefinedException("Nieprawidłowe imię");
+        }
+        if (checkName(lastName)) {
+            throw new NameUndefinedException("Nieprawidłowe naziwsko");
+        }
+        if (checkAge(age)) {
+            throw new IncorrectAgeException("Nieprawidłowy wiek");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public void setFirstName(String firstName) {
+        if (checkName(firstName)) {
+            throw new NameUndefinedException("Nieprawidłowe imię");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (checkName(lastName)) {
+            throw new NameUndefinedException("Nieprawidłowe naziwsko");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (checkAge(age)) {
+            throw new IncorrectAgeException("Nieprawidłowy wiek");
+        }
+        this.age = age;
+    }
+
+    private boolean checkName(String firstName) {
+        return firstName == null || firstName.length() < 2;
+    }
+
+    private boolean checkAge(int age) {
+        return age < 1;
+    }
+}
